bc838bc9108d2a5e9dedd209ecf36bdff107ceb4
ryzenmagsino <magsino301107@gmail.com>
2024-11-27 11:42:47 +0800

    act4

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 
@@ -18,7 +19,17 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for the request
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port', process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+      //listen for the request
+    app.listen(process.env.PORT, () => {
+        console.log('Server is running on port', process.env.PORT)
+    })
+
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
